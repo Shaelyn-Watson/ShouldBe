d42909184e8247837f21566d51c31a0a00579e7e
shaelyn-watson <shaelynwatson@shaelyns-imac.gateway.2wire.net>
2014-03-12 22:56:03 -0500

    fixed names in Manifest

--- src/app/there/shouldbe/TapActivity.java
+++ src/app/there/shouldbe/TapActivity.java
@@ -18,12 +18,30 @@ public class TapActivity extends Activity {
             // Show the Up button in the action bar.
             getActionBar().setDisplayHomeAsUpEnabled(true);
         }
+        
+        
+        //======
+        
+        // Get a handle to the Map Fragment
+//        GoogleMap map = ((MapFragment) getFragmentManager()
+//                .findFragmentById(R.id.map)).getMap();
+//
+//        LatLng sydney = new LatLng(-33.867, 151.206);
+//
+//        map.setMyLocationEnabled(true);
+//        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+//
+//        map.addMarker(new MarkerOptions()
+//                .title("Sydney")
+//                .snippet("The most populous city in Australia.")
+//                .position(sydney));
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
         case android.R.id.home:
+        	this.finish();
             NavUtils.navigateUpFromSameTask(this);
             return true;
         }
