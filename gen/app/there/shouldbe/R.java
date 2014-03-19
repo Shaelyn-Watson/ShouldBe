9f1bb96fa40fb706cd1db9f5a7b635b424a0acf9
Alexis <aemperador83@yahoo.com>
2014-03-19 13:32:02 -0500

    removed bin

--- gen/app/there/shouldbe/R.java
+++ gen/app/there/shouldbe/R.java
@@ -24,16 +24,27 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090009;
-        public static final int button1=0x7f090004;
-        public static final int button2=0x7f090005;
-        public static final int post_wall=0x7f090002;
-        public static final int tap_loc=0x7f090001;
-        public static final int textView1=0x7f090000;
-        public static final int tweet_date=0x7f090006;
-        public static final int tweet_message=0x7f090007;
-        public static final int tweet_name=0x7f090008;
-        public static final int twitterfeed=0x7f090003;
+        public static final int action_forgot_password=0x7f090014;
+        public static final int action_settings=0x7f090013;
+        public static final int button1=0x7f090007;
+        public static final int button2=0x7f090008;
+        public static final int buttonGenPost=0x7f090002;
+        public static final int email=0x7f09000c;
+        public static final int etGenPost=0x7f090001;
+        public static final int login=0x7f09000e;
+        public static final int login_form=0x7f09000b;
+        public static final int login_status=0x7f090009;
+        public static final int login_status_message=0x7f09000a;
+        public static final int password=0x7f09000d;
+        public static final int post_wall=0x7f090004;
+        public static final int sign_in_button=0x7f09000f;
+        public static final int tap_loc=0x7f090003;
+        public static final int textView1=0x7f090006;
+        public static final int tvGenPost=0x7f090000;
+        public static final int tweet_date=0x7f090010;
+        public static final int tweet_message=0x7f090011;
+        public static final int tweet_name=0x7f090012;
+        public static final int twitterfeed=0x7f090005;
     }
     public static final class integer {
         public static final int google_play_services_version=0x7f050000;
@@ -43,18 +54,35 @@ public final class R {
         public static final int activity_home=0x7f030001;
         public static final int activity_tap_location=0x7f030002;
         public static final int activity_twitter_feed=0x7f030003;
-        public static final int list_item=0x7f030004;
+        public static final int activity_twitter_login=0x7f030004;
+        public static final int list_item=0x7f030005;
     }
     public static final class menu {
         public static final int home=0x7f080000;
+        public static final int twitter_login=0x7f080001;
     }
     public static final class string {
+        public static final int action_forgot_password=0x7f06000e;
         public static final int action_settings=0x7f060001;
+        public static final int action_sign_in_register=0x7f06000c;
+        public static final int action_sign_in_short=0x7f06000d;
         public static final int app_name=0x7f060000;
+        public static final int error_field_required=0x7f060013;
+        public static final int error_incorrect_password=0x7f060012;
+        public static final int error_invalid_email=0x7f060010;
+        public static final int error_invalid_password=0x7f060011;
+        public static final int etGenPostPrompt=0x7f060008;
         public static final int hello_world=0x7f060002;
+        public static final int login_progress_signing_in=0x7f06000f;
         public static final int post_wall=0x7f060004;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f06000a;
+        public static final int prompt_password=0x7f06000b;
         public static final int tap_location=0x7f060003;
         public static final int title_activity_twitter_feed=0x7f060005;
+        public static final int title_activity_twitter_login=0x7f060009;
+        public static final int tvGenPostText=0x7f060007;
         public static final int twitter_feed=0x7f060006;
     }
     public static final class style {
@@ -84,5 +112,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
+        public static final int LoginFormContainer=0x7f070002;
     }
 }
