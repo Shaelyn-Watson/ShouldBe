b26ab9f8dc5713ab44e897c37cefb4e4d31f3e77
Shaelyn-Watson <shaelynjoy@gmail.com>
2014-03-25 14:25:03 -0500

    zoom to GDC Mike code

--- src/app/there/shouldbe/TapActivity.java
+++ src/app/there/shouldbe/TapActivity.java
@@ -1,19 +1,47 @@
 package app.there.shouldbe;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
-//import com.google.android.gms.maps.GoogleMap;
-//import com.google.android.gms.maps.SupportMapFragment;
-
-public class TapActivity extends FragmentActivity {
+public class TapActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    	//Log.d("play_services", "is google play services available = " +  GooglePlayServicesUtil.isGooglePlayServicesAvailable(null));
         super.onCreate(savedInstanceState);
-        //GoogleMap mMap = ((SupportMapFragment) getSupportFragmentManager()
-                //.findFragmentById(R.id.map)).getMap();
         setContentView(R.layout.activity_tap_location);
     }
+
+
+    public void startMap(View v) {
+
+        // lat and long for Gates Computer Science Complex:
+        double latitude = 30.286336;
+        double longitude = -97.736693;
+
+
+        String oldAddress = "geo:0,0?q=713+n+duchesne+st.+charles+mo";
+        String gatesAddress = "geo:0,0?q=2317+speedway+austin+tx";
+        
+         // default zoom, no label
+//	         String uri = String.format(Locale.ENGLISH,"geo:%f,%f",latitude,longitude);
+//	         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(oldAddress));
+//	         startActivity(intent);
+
+
+
+        // with label and given zoom
+        String label = "Bill and Melinda Gates Computer Science Complex";
+        String uriBegin = "geo:" + latitude + "," + longitude;
+        String query = latitude + "," + longitude + "(" + label + ")";
+        String encodedQuery = Uri.encode(query);
+        String uriString = uriBegin + "?q=" + encodedQuery + "&z=18";
+        Uri uri = Uri.parse(uriString);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        this.startActivity(intent);
+    }
+
+	
 }
