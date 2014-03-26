df7073b25b44ae7d13c18feb243d64487c781eed
Shaelyn-Watson <shaelynjoy@gmail.com>
2014-03-26 15:50:32 -0500

    back to mainactivity from map

--- src/app/there/shouldbe/TapActivity.java
+++ src/app/there/shouldbe/TapActivity.java
@@ -1,6 +1,9 @@
 package app.there.shouldbe;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,6 +23,12 @@ public class TapActivity extends MapActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tap_location);
         
+        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // Show the Up button in the action bar.
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        
         mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         mMap.setMyLocationEnabled(true);
         setUpMapIfNeeded();
@@ -38,6 +47,15 @@ public class TapActivity extends MapActivity {
 
     }
     
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
