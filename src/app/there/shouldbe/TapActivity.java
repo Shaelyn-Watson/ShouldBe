914b877804ae55134ac7502442f9cd2bfd1beeff
Shaelyn-Watson <shaelynjoy@gmail.com>
2014-03-26 11:48:08 -0500

    Google map instance created

--- src/app/there/shouldbe/TapActivity.java
+++ src/app/there/shouldbe/TapActivity.java
@@ -1,47 +1,52 @@
 package app.there.shouldbe;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 
-public class TapActivity extends Activity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.maps.MapActivity;
+
+public class TapActivity extends MapActivity {
+	
+	private GoogleMap mMap;
+	private static final LatLng GDC = new LatLng(30.286336,-97.736693);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tap_location);
-    }
-
-
-    public void startMap(View v) {
-
-        // lat and long for Gates Computer Science Complex:
-        double latitude = 30.286336;
-        double longitude = -97.736693;
-
-
-        String oldAddress = "geo:0,0?q=713+n+duchesne+st.+charles+mo";
-        String gatesAddress = "geo:0,0?q=2317+speedway+austin+tx";
         
-         // default zoom, no label
-//	         String uri = String.format(Locale.ENGLISH,"geo:%f,%f",latitude,longitude);
-//	         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(oldAddress));
-//	         startActivity(intent);
+        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        mMap.setMyLocationEnabled(true);
+        setUpMapIfNeeded();
+        manipulateMap();
 
-
-
-        // with label and given zoom
-        String label = "Bill and Melinda Gates Computer Science Complex";
-        String uriBegin = "geo:" + latitude + "," + longitude;
-        String query = latitude + "," + longitude + "(" + label + ")";
-        String encodedQuery = Uri.encode(query);
-        String uriString = uriBegin + "?q=" + encodedQuery + "&z=18";
-        Uri uri = Uri.parse(uriString);
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        this.startActivity(intent);
     }
+    
+    
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                                .getMap();
+        }
+    }
+
+
+    private void manipulateMap() {
+    	// Move the camera instantly to Sydney with a zoom of 15.
+    	mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(GDC, 15));
+
+	}
+
+
+	@Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
+    
 
 	
 }
