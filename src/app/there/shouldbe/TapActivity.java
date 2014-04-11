c06b066a7c7077cce8602656c287c0e83358cd82
Shaelyn-Watson <shaelynjoy@gmail.com>
2014-04-11 00:11:28 -0500

    button in map pin popups

--- src/app/there/shouldbe/TapActivity.java
+++ src/app/there/shouldbe/TapActivity.java
@@ -1,6 +1,7 @@
 package app.there.shouldbe;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Build;
@@ -8,6 +9,9 @@ import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -15,6 +19,7 @@ import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -33,6 +38,13 @@ public class TapActivity extends MapActivity implements
 	private Location mCurrentLocation;
 	private LatLng mCurrentLatLng;
 
+	//info window
+	private ViewGroup infoWindow;
+    private TextView infoTitle;
+    private TextView infoSnippet;
+    private Button infoButton;
+    private OnInfoWindowElemTouchListener infoButtonListener;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,16 +58,18 @@ public class TapActivity extends MapActivity implements
         
         mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         mMap.setMyLocationEnabled(true);
+        final MapWrapperLayout mapWrapperLayout = (MapWrapperLayout)findViewById(R.id.map_relative_layout);
         mLocationClient = new LocationClient(this, this, this);
-        setUpMapIfNeeded();
-//        manipulateMap();
-//        zoomToUserLocation();
+        setUpMapIfNeeded(); //ensure map loads
         
+        /*
+         * TODO
+         * click on map = show existing pin or create new 
+         * 
+         * */
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-
             @Override
             public void onMapClick(LatLng point) { 
-                
                 Marker shouldBePin = mMap.addMarker(new MarkerOptions().position(point)
                 	.icon(BitmapDescriptorFactory.fromResource(R.drawable.shouldbepin))
                 	.title("There should be:")
@@ -65,7 +79,53 @@ public class TapActivity extends MapActivity implements
             }
         });
         
-        mMap.setInfoWindowAdapter(new IconizedWindowAdapter(getLayoutInflater()));   
+        /*
+         * TODO
+         * click on infowindow 
+         * 
+         * */
+        // MapWrapperLayout initialization
+        // 39 - default marker height
+        // 20 - offset between the default InfoWindow bottom edge and it's content bottom edge 
+        mapWrapperLayout.init(mMap, getPixelsFromDp(this, 39 + 20)); 
+
+        this.infoWindow = (ViewGroup)getLayoutInflater().inflate(R.layout.map_info_window, null);
+        this.infoTitle = (TextView)infoWindow.findViewById(R.id.title);
+        //this.infoSnippet = (TextView)infoWindow.findViewById(R.id.snippet);
+        this.infoButton = (Button)infoWindow.findViewById(R.id.button);
+
+        // Setting custom OnTouchListener which deals with the pressed state 
+        this.infoButtonListener = new OnInfoWindowElemTouchListener(infoButton,
+                getResources().getDrawable(R.drawable.twitter),
+                getResources().getDrawable(R.drawable.twitter)) 
+        {
+            @Override
+            protected void onClickConfirmed(View v, Marker marker) {
+                // Here we can perform some action triggered after clicking the button
+                Toast.makeText(TapActivity.this, marker.getTitle() + "'s button clicked!", Toast.LENGTH_SHORT).show();
+            }
+        }; 
+        this.infoButton.setOnTouchListener(infoButtonListener);
+
+        mMap.setInfoWindowAdapter(new InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                // Setting up the infoWindow with current's marker info
+                infoTitle.setText(marker.getTitle());
+                //infoSnippet.setText(marker.getSnippet());
+                infoButtonListener.setMarker(marker);
+
+                // We must call this to set the current marker and infoWindow references
+                // to the MapWrapperLayout
+                mapWrapperLayout.setMarkerWithInfoWindow(marker, infoWindow);
+                return infoWindow;
+            }
+        });  
 
     }
     
@@ -84,6 +144,11 @@ public class TapActivity extends MapActivity implements
         if (mMap == null) {
             mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         }
+    }
+    
+    public static int getPixelsFromDp(Context context, float dp) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int)(dp * scale + 0.5f);
     }
 
     @Override
@@ -116,8 +181,7 @@ public class TapActivity extends MapActivity implements
     }
 	
 	@Override
-    protected void onActivityResult(
-            int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // Decide what to do based on the original request code
         switch (requestCode) {
             case CONNECTION_FAILURE_RESOLUTION_REQUEST :
