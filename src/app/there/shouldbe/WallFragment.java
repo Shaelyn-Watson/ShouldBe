980ca8f781df69179177b4336fd7c21c3268ffee
Shaelyn-Watson <shaelynjoy@gmail.com>
2014-04-25 17:47:33 -0500

    wall is fragment

--- src/app/there/shouldbe/WallFragment.java
+++ src/app/there/shouldbe/WallFragment.java
@@ -1,20 +1,58 @@
 package app.there.shouldbe;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import twitter4j.AsyncTwitter;
+import twitter4j.AsyncTwitterFactory;
+import twitter4j.Paging;
+import twitter4j.ResponseList;
+import twitter4j.Status;
+import twitter4j.TwitterAdapter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterListener;
+import twitter4j.TwitterMethod;
+import twitter4j.conf.ConfigurationBuilder;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.ListFragment;
+import android.support.v4.app.NavUtils;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class WallFragment extends ListFragment {
-	
-	String[] numbers_text = new String[] { "one", "two", "three", "four",  
-		    "five", "six", "seven", "eight", "nine", "ten", "eleven",  
-		    "twelve", "thirteen", "fourteen", "fifteen" };  
-		  String[] numbers_digits = new String[] { "1", "2", "3", "4", "5", "6", "7",  
-		    "8", "9", "10", "11", "12", "13", "14", "15" };  
+public class WallFragment extends ListFragment { 	  
+
+			private AsyncTwitter twitter;
+			private final String CONSUMER_KEY = "7TKDKSkU8e1DiF2oLTdA";
+			private final String CONSUMER_SECRET = "jRtJeN2NSXmxfAB8TV2YtS11dYrVkHQ8mG7tOxOXXw";
+			private final String ACCESS_KEY = "2360041674-Z2lfohxkkx3ZCeNEldLwLP81VXk8eB6rH7PKMsc";
+			private final String ACCESS_SECRET = "gDznLbH7tnYXwjaW53uqon33pKMcrIV6OYxVi2Y6nU7Zh";
+			private final long SHOULDBE_TWITTER_ID = 2360041674L; // L is required for long value - not a part of ID
+			ArrayList<Tweet> tweetList = new ArrayList<Tweet>();
+		  
+		  @Override
+			public void onCreate(Bundle savedInstanceState) {
+				super.onCreate(savedInstanceState);
+				//setContentView(R.layout.activity_twitter_feed);
+				StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+				StrictMode.setThreadPolicy(policy);
+				
+				// Make sure we're running on Honeycomb or higher to use ActionBar APIs
+		        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+		            // Show the Up button in the action bar.
+		            //getActionBar().setDisplayHomeAsUpEnabled(true);
+		        }
+		        //getIntent().setAction("Already Created");
+		        //view = getListView();
+		        setAdapter();
+			}
 		  
 		  @Override  
 		  public void onListItemClick(ListView l, View v, int position, long id) {  
@@ -24,17 +62,98 @@ public class WallFragment extends ListFragment {
 		  @Override  
 		  public View onCreateView(LayoutInflater inflater, ViewGroup container,  
 		    Bundle savedInstanceState) {  
-		   ArrayAdapter<String> adapter = new ArrayAdapter<String>(  
+		   ArrayAdapter<Tweet> adapter = new ArrayAdapter<Tweet>(  
 		     inflater.getContext(), android.R.layout.simple_list_item_1,  
-		     numbers_text);  
+		     tweetList);  
 		   setListAdapter(adapter);  
 		   return super.onCreateView(inflater, container, savedInstanceState);  
-		  }  
-		   
-	
-//	  @Override
-//	  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//	    View view = inflater.inflate(R.layout.activity_twitter_feed, null);
-//	    return view;
-//	  }
+		  }
+		  
+			
+			private void setAdapter() {
+				ConfigurationBuilder cb = new ConfigurationBuilder();
+			    cb.setDebugEnabled(true)
+			            .setOAuthConsumerKey(CONSUMER_KEY)
+			            .setOAuthConsumerSecret(CONSUMER_SECRET)
+			            .setOAuthAccessToken(ACCESS_KEY)
+			            .setOAuthAccessTokenSecret(ACCESS_SECRET);
+			    AsyncTwitterFactory tf = new AsyncTwitterFactory(cb.build());
+				twitter = tf.getInstance();
+				getTimeLine();
+			}
+			
+			/**
+			 * Force restart onResume() to refresh twitter feed
+			 */
+			@Override
+			public void onResume() {
+//				String action = getIntent().getAction();
+//				if (action == null || !action.equals("Already Created")){
+//					Intent intent = new Intent(this, TwitterFeedActivity.class);
+//					startActivity(intent);
+//					this.finish();
+//				}
+//				else {
+//					getIntent().setAction(null);
+//				}
+						
+				super.onResume();
+			}
+			
+			public void getTimeLine() {
+				try {
+					TwitterListener listener = new TwitterAdapter() {
+						@Override
+						public void gotUserTimeline(ResponseList<Status> statuses) {
+							for(Status st : statuses) {
+								tweetList.add(new Tweet((st)));
+							}
+						}
+						
+						@Override
+						public void gotMentions(ResponseList<Status> statuses) {
+							for(Status st: statuses) {
+								tweetList.add(new Tweet((st)));
+							}
+							displayTwitter(tweetList);
+						}
+
+						@Override
+						public void onException(TwitterException e, TwitterMethod method) {
+						    if(method == TwitterMethod.HOME_TIMELINE) {
+								e.printStackTrace();
+							} 
+						    else {
+							    e.printStackTrace();
+							}
+						}
+					};
+					
+					twitter.addListener(listener);
+					Paging paging = new Paging();
+					paging.setCount(20);
+					twitter.getMentions(paging);
+				}
+				catch(Exception ex) {
+					ex.printStackTrace();
+					System.out.println("Failed to get timeline " + ex.getMessage());
+				}
+			}
+			
+			private void displayTwitter(final ArrayList<Tweet> tweetList) {
+				Collections.sort(tweetList, new Comparator<Tweet>() {
+					public int compare(Tweet t1, Tweet t2) {
+						return t1.getDate().compareTo(t2.getDate());
+					}
+				});
+			
+				final TweetAdapter<Tweet> adapter = new TweetAdapter<Tweet>(getActivity(), tweetList);
+		      
+				getActivity().runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						setListAdapter(adapter);
+					}
+				});
+			}
 }
