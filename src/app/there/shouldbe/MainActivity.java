d5781aed4b205b94ec48b6ca46fe59db7fdbbfa4
Shaelyn-Watson <shaelynjoy@gmail.com>
2014-04-25 22:55:11 -0500

    tabs instead of swipes

--- src/app/there/shouldbe/MainActivity.java
+++ src/app/there/shouldbe/MainActivity.java
@@ -1,5 +1,7 @@
 package app.there.shouldbe;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -11,65 +13,112 @@ import android.support.v4.view.ViewPager;
 
 public class MainActivity extends FragmentActivity {
 
-	  public static final int WALL_ACTIVITY = 0;
-	  public static final int MAP_ACTIVITY = 1;
-	  public static final int TWEET_ACTIVITY = 2;
 	  private static final int NUM_PAGES = 2;
 	  private PagerAdapter mPagerAdapter;   //The adapter definition of the fragments
 	  private ViewPager mPager;  //The ViewPager that hosts the section contents
 
+	  private ScreenSlidePagerAdapter pagerAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_screen_slide);
 
+		final ActionBar actionBar = getActionBar();
 	    // Instantiate a ViewPager and a PagerAdapter.
-	    mPager = (ViewPager) findViewById(R.id.pager);
-	    mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
-	    mPager.setAdapter(mPagerAdapter);
-	    mPager.setCurrentItem(0);
-}
+		pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(pagerAdapter);
+	    
+	    mPager.setOnPageChangeListener(
+	            new ViewPager.SimpleOnPageChangeListener() {
+	                @Override
+	                public void onPageSelected(int position) {
+	                    // When swiping between pages, select the
+	                    // corresponding tab.
+	                    getActionBar().setSelectedNavigationItem(position);
+	                }
+	            });
+	    //mPager.setAdapter(mPagerAdapter);
+	    //mPager.setCurrentItem(0);
+	    
+	    // Specify that tabs should be displayed in the action bar.
+	    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
+	    // Create a tab listener that is called when the user changes tabs.
+	    ActionBar.TabListener tabListener = new ActionBar.TabListener() {
 
-@Override
-public void onBackPressed() {
-    if (mPager.getCurrentItem() == 0) {
-        // If the user is currently looking at the first step, allow the system to handle the
-        // Back button. This calls finish() on this activity and pops the back stack.
-        super.onBackPressed();
-    } else {
-        // Otherwise, select the previous step.
-    	//Toast.makeText(MainActivity.this, " back to old frag " + String.valueOf(mPager.getCurrentItem() - 1), Toast.LENGTH_SHORT).show();
-        mPager.setCurrentItem(mPager.getCurrentItem() - 1);
-    }
-}
+			@Override
+			public void onTabReselected(Tab arg0,
+					android.app.FragmentTransaction arg1) {
+				// probably ignore this event
+				
+			}
 
-/**
- * A simple pager adapter that represents ScreenSlidePageFragment objects, in
- * sequence.
- */
-private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
-    public ScreenSlidePagerAdapter(FragmentManager fm) {
-        super(fm);
-    }
+			@Override
+			public void onTabSelected(Tab tab,
+					android.app.FragmentTransaction arg1) {
+				// show the given tab
+	        	mPager.setCurrentItem(tab.getPosition());
+				
+			}
 
-    @Override
-    public Fragment getItem(int position) {
-    	if(position == 0){
-    		//wall activity
-    		return new WallFragment();
-    	}
-    	else{
-    		return new ShouldBeFragment();
-    	}
-    }
+			@Override
+			public void onTabUnselected(Tab arg0,
+					android.app.FragmentTransaction arg1) {
+				// hide the given tab
+				
+			}
+	    };
 
-    @Override
-    public int getCount() {
-        return NUM_PAGES;
-    }
-}
+	    // Add 3 tabs, specifying the tab's text and TabListener
+	    for (int i = 0; i < 2; i++) {
+	        actionBar.addTab(
+	                actionBar.newTab()
+	                        .setText("Tab " + (i + 1))
+	                        .setTabListener(tabListener));
+	    }
+	}
+	
+	
+	@Override
+	public void onBackPressed() {
+	    if (mPager.getCurrentItem() == 0) {
+	        // If the user is currently looking at the first step, allow the system to handle the
+	        // Back button. This calls finish() on this activity and pops the back stack.
+	        super.onBackPressed();
+	    } else {
+	        // Otherwise, select the previous step.
+	    	//Toast.makeText(MainActivity.this, " back to old frag " + String.valueOf(mPager.getCurrentItem() - 1), Toast.LENGTH_SHORT).show();
+	        mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+	    }
+	}
+	
+	/**
+	 * A simple pager adapter that represents ScreenSlidePageFragment objects, in
+	 * sequence.
+	 */
+	private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+	    public ScreenSlidePagerAdapter(FragmentManager fm) {
+	        super(fm);
+	    }
+	
+	    @Override
+	    public Fragment getItem(int position) {
+	    	if(position == 0){
+	    		//wall activity
+	    		return new WallFragment();
+	    	}
+	    	else{
+	    		return new ShouldBeFragment();
+	    	}
+	    }
+	
+	    @Override
+	    public int getCount() {
+	        return NUM_PAGES;
+	    }
+	}
 
 
 }
