253fdf36f814ea74694fbf068d5210366cfcb135
Alexis <aemperador83@yahoo.com>
2014-06-29 22:40:16 -0500

    redirecting to login page. started work on login and sign up buttons

--- src/app/there/shouldbe/UserLogin.java
+++ src/app/there/shouldbe/UserLogin.java
@@ -1,27 +1,62 @@
 package app.there.shouldbe;
 
+import java.util.List;
+
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 public class UserLogin extends Activity {
+	
+	Button signUp;
+	Button login;
+	EditText usernameET;
+	EditText passwordET;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_user_login);
-
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		
+		signUp = (Button) findViewById(R.id.signupButton);
+		signUp.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(UserLogin.this, SignUp.class);
+				startActivity(intent);
+				finish();
+			}
+		});
+		
+		login = (Button) findViewById(R.id.loginButton);
+		login.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				checkCredentials();
+			}
+		});
+		
+		usernameET = (EditText) findViewById(R.id.usernameET);
+		passwordET = (EditText) findViewById(R.id.passwordET);
+		
+		// ========================
+        // ======== Parse =========
+        // ========================
+        Parse.initialize(this, "3wJJsTSZovcJZreXDjYVeJi3e1AOqAZEA8e2S860", "y1SZ9RtY8wuv9sOaXTIHrapLK5uk6LrehEEYylZd");
 	}
 
 	@Override
@@ -43,22 +78,31 @@ public class UserLogin extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
+	
+	public void checkCredentials() {
+		try {
+			ParseQuery<ParseObject> query = ParseQuery.getQuery("Profiles");
+			query.whereEqualTo("username", usernameET);
+			query.setLimit(1);
+			List<ParseObject> temp = query.find();
+			if (temp.get(0).equals(usernameET)) {
+				query.whereEqualTo("password", passwordET);
+				query.setLimit(1);
+				temp = query.find();
+				if (temp.get(0).equals(passwordET)) {
+					System.out.println("Success signed in!"); // start here next time to handle code for signing in
+				}
+			}
+			else {
+				System.out.println("Username/password combination not found"); // fix to print to screen
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
 		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_user_login,
-					container, false);
-			return rootView;
-		}
+	
+		
+		
+		
 	}
 
 }
